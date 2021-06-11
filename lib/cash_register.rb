4982d79e2bad8fb98bbbbc7167c0a01f1afa1648
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-11 18:49:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,7 @@ class CashRegister
   attr_accessor :total, :items, :discount, :last_item
   
   
-  def initialize(discount = nil, )
+  def initialize(discount = nil, total = 0, )
     @total = 0 
     @discount = 0 
     @items = 0 
