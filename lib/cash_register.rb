77ff71cf8ae78d6c0fcdabec0b68b6d7f7abab7a
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-11 19:05:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
   def add_item(item, price, quantity = 1)
     self.last_item = price * quantity
     self.total = (price * quantity)
-    
+    quantity.times ()
   end 
   
 end
